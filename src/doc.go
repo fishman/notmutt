206ea0b23fafdcb239f14ef6006828fa48f8949c
Reza Jelveh <git@jelveh.me>
2026-08-14 01:16:32 +0800

    Scaffold Go module with pinned, vendored dependencies

---
+++ src/doc.go
@@ -0,0 +1,9 @@
+package notmutt
+
+import (
+	_ "github.com/BurntSushi/toml"
+	_ "github.com/charmbracelet/bubbletea"
+	_ "github.com/emersion/go-message"
+	_ "github.com/mattn/go-runewidth"
+	_ "go.etcd.io/bbolt"
+)
